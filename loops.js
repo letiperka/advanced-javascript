75c964296fb4226e091af2ad6d5f400120a20857
anja <119950303+letiperka@users.noreply.github.com>
2023-01-26 21:44:50 +0100

    added files

---
+++ loops.js
@@ -0,0 +1,30 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+const users = ["Max", "Anna", "Joel"];
+
+console.log(users);
+
+for (const user of users) {
+    console.log(user);
+}
+
+const loggedInUser = {
+    name: "Max",
+    age: 32,
+    isAdmin: true
+};
+
+for (const key in loggedInUser) {
+    console.log(key);
+    console.log(loggedInUser[key])
+}
+
+let isFinished = false;
+
+while (!isFinished) {
+    isFinished = confirm('Do you want to quit?');
+}
+
+console.log('Done!');
